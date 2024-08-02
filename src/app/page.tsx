d59efcdcdb286cf7aca393437033992b92adf7b5
Alejandro Echavarria <aechavarriaj@gmail.com>
2024-08-02 17:45:56 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Hero from "../../components/hero";
+import { LoginForm } from "../../components/loginForm";
+import Navbar from "../../components/navbar";
+import { RegisterForm } from "../../components/registerForm";
+import SearchBar from "../../components/searchbar";
 export default function Home() {
   return (
     <main>
       <Hero/>
+      <SearchBar/>
     </main>
   );
 }
